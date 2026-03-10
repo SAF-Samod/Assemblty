bb58b419410da15156872c13b236df45a72c11fd
SAF-Samod <samodabhimana@gmail.com>
2026-03-11 03:08:46 +0530

    Code optimization ( there's no need to move the number to buffer (r15) , because it already happened in the previous iteration)

--- practise x86.asm
+++ practise x86.asm
@@ -17,42 +17,33 @@ mov r15, rbx
 add r15, rcx
 mov rdx, r15
 
-mov r15, rdx
 add r15, rcx
 mov rsi, r15
 
-mov r15, rsi
 add r15, rdx
 mov rdi, r15
 
-
-mov r15, rdi
 add r15, rsi
 mov r8, r15
 
-mov r15, r8
 add r15, rdi
 mov r9, r15
 
-mov r15, r9
 add r15, r8
 mov r10, r15
 
-mov r15, r10
 add r15, r9
 mov r11, r15
 
-mov r15, r11
 add r15, r10
 mov r12, r15
 
-mov r15, r12
 add r15, r12
 mov r13, r15
 
-mov r15, r13
 add r15, r13
 mov r14, r15
 
 mov r15, r14
 add r15, r13
+
